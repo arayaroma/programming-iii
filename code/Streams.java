ecafb1d1951461ee451448254be625057d53649b
Daniel Araya <darayaroma@gmail.com>
2023-08-05 22:28:36 -0600

    [added] to-do streams

---
+++ code/Streams.java
@@ -0,0 +1,25 @@
+package code;
+
+/*
+ * To do:
+ * 1. filter(Predicate)
+ * 2. allMatch(Predicate)
+ * 3. anyMatch(Predicate)
+ * 4. min(Comparator)
+ * 5. max(Comparator)
+ * 6. map(Function)
+ * 7. reduce(T, BiFunction)
+ * 8. forEach(Consumer)
+ * 9. sorted(Comparator)
+ * 10. collect(Collector)
+ * 11. toList
+ * 12. toSet
+ * 13. groupingBy(Function)
+ * 14. partitioningBy(Predicate)
+ * 15. groupingBy(Function, Collector)
+ * 16. groupingBy(Function, Supplier, Collector)
+ */
+
+public class Streams {
+    
+}
