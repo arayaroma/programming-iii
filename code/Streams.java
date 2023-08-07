61053a9ccfc550b61a2f1a5e3a6abad4ec0d1c1b
Daniel Araya <darayaroma@gmail.com>
2023-08-07 14:19:08 -0600

    [add] filter(Predicate) getFlightWhereCapacityIsMoreThan300

--- code/Streams.java
+++ code/Streams.java
@@ -1,8 +1,13 @@
 package code;
 
+import java.time.LocalDate;
+import java.util.List;
+
 /*
+* Done:
+* 1. filter(Predicate)
+* 
  * To do:
- * 1. filter(Predicate)
  * 2. allMatch(Predicate)
  * 3. anyMatch(Predicate)
  * 4. min(Comparator)
@@ -21,5 +26,86 @@ package code;
  */
 
 public class Streams {
-    
+    public static void main(String[] args) {
+        Driver();
+    }
+
+    public static void Driver() {
+        MockedFlight();
+    }
+
+    public static void MockedFlight() {
+        List<Flight> flights = createFlights();
+        System.out.println("Total flights: ");
+        flights.forEach(flight -> System.out.println(flight.toString()));
+
+        List<Flight> filteredFlights = getFlightWhereCapacityIsMoreThan300(flights);
+
+        System.out.println("Filtered flights: ");
+        filteredFlights.forEach(flight -> System.out.println(flight.toString()));
+    }
+
+    public static List<Flight> createFlights() {
+        List<Flight> flights = new java.util.ArrayList<>();
+        Flight flight1 = new Flight(1, "London", 100, 200, LocalDate.of(2020, 1, 30));
+        Flight flight2 = new Flight(2, "New Jersey", 200, 250, LocalDate.of(2020, 1, 15));
+        Flight flight3 = new Flight(3, "Alajuela", 250, 300, LocalDate.of(2020, 2, 3));
+        Flight flight4 = new Flight(4, "Helsinski", 300, 350, LocalDate.of(2020, 2, 5));
+        Flight flight5 = new Flight(5, "Tokyo", 350, 400, LocalDate.of(2020, 2, 10));
+        flights.add(flight1);
+        flights.add(flight2);
+        flights.add(flight3);
+        flights.add(flight4);
+        flights.add(flight5);
+        return flights;
+    }
+
+    public static List<Flight> getFlightWhereCapacityIsMoreThan300(List<Flight> flights) {
+        return flights.stream()
+                .filter(flight -> flight.getCapacity() > 300)
+                .collect(java.util.stream.Collectors.toList());
+    }
+
+}
+
+class Flight {
+    private int number;
+    private String destination;
+    private int price;
+    private int capacity;
+    private LocalDate date;
+
+    public Flight(int number, String destination, int price, int capacity, LocalDate date) {
+        this.number = number;
+        this.destination = destination;
+        this.price = price;
+        this.capacity = capacity;
+        this.date = date;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String toString() {
+        return "Flight number: " + number + ", destination: " + destination + ", price: " + price + ", capacity: "
+                + capacity + ", date: " + date;
+    }
+
 }
